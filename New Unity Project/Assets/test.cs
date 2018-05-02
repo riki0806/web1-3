7825228e39b6f5900a3326664a61d070d00faf90
riki0806 <michitaka0806@gmail.com>
2018-05-02 17:47:42 +0900

    マジ卍

---
+++ New Unity Project/Assets/test.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int sum = 0;
+        for(int i = 1; i <= 10; i++)
+        {
+            sum += i;
+        }
+        Debug.Log(sum);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
